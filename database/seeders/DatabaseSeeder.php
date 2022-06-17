ec68516545663d4910fbba3ef6a056dc5741ffd9
John Doe <https://johngit002@github.com>
2022-06-17 16:02:26 +0300

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,22 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            //SalaryTypesSeeder::class,
-            //SEODepartmentSeeder::class,
-            //SEOEmployeeSeeder::class,
-            //QADepartmentSeeder::class,
-            //QAPerHourDepartmentSeeder::class,
-            //QAEmploeeSeeder::class,
-            //ProgrammersDepartmentMonthlySeeder::class,
-            //ProgrammersDepartmentPerHourSeeder::class,
-            //ProgrammersEmploeeSeeder::class
+            SalaryTypesSeeder::class,
+            SEODepartmentSeeder::class,
+            SEOEmployeeSeeder::class,
+            QADepartmentSeeder::class,
+            QAPerHourDepartmentSeeder::class,
+            QAEmploeeSeeder::class,
+            ProgrammersDepartmentMonthlySeeder::class,
+            ProgrammersDepartmentPerHourSeeder::class,
+            ProgrammersEmploeeSeeder::class
         ]);
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
